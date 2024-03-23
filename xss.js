ac2ac3dcd736ca3e42006d7c3c436a302c62de9b
e1stdark <131532873+e1stdark@users.noreply.github.com>
2024-03-23 14:29:48 +0800

    Create xss.js

---
+++ xss.js
@@ -0,0 +1,10 @@
+import { urlencoded } from "body-parser";
+import ejs from "ejs";
+import express from "express";
+import session from "express-session";
+import crypto from "node:crypto";
+import * as fs from "node:fs";
+import visit from "./bot";
+import { sha256 } from "./utils";
+import ./app
+alert(flag.content());
